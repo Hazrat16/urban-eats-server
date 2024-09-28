7d90e34b03f24e5ec728c5ea452dfcb292f2b26c
hazrat <hazrat17016@gmail.com>
2024-09-28 12:14:45 +0600

    Sepatation dev  and prod env

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,10 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t04cp.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  process.env.NODE_ENV === "production"
+    ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t04cp.mongodb.net/?retryWrites=true&w=majority`
+    : "mongodb://127.0.0.1:27017/urban-eats";
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
